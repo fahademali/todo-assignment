b02cd3d71abf60bf2c99a798f03b7c441ca316bd
fahad ali <fahad.ali@emumba.com>
2023-10-15 17:52:02 +0500

    Add infrastructure to the app

--- user_service/cmd/server/main.go
+++ user_service/cmd/server/main.go
@@ -4,12 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"user_service/apis/routes"
+	"user_service/infra"
 )
 
 func main() {
 	r := gin.Default()
 
 	routes.AddUserRoutes(r)
+	infra.DbConnection()
 
 	// r.GET("/ping", func(ctx *gin.Context) {
 	// 	ctx.JSON(200, gin.H{
--- user_service/infra/db.go
+++ user_service/infra/db.go
@@ -1,1 +1,37 @@
 package infra
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func DbConnection() {
+	connectionStr := "user=user password=mypassword dbname=user sslmode=disable"
+	conn, err := sql.Open("postgres", connectionStr)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(conn)
+	createTableSQL := `
+		CREATE TABLE IF NOT EXISTS users (
+			id SERIAL PRIMARY KEY,
+			username VARCHAR(50) NOT NULL,
+			email VARCHAR(100) NOT NULL
+		)
+	`
+	rows, err := conn.Query("SELECT version();")
+	_, err2 := conn.Exec(createTableSQL)
+
+	if err2 != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		var version string
+		rows.Scan(&version)
+		fmt.Println("version")
+		fmt.Println(version)
+	}
+}
